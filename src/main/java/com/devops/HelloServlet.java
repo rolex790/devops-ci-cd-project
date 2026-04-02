135f4d2a61bed8c1dd0e0bde083d23392afb93a6
rolex790 <yokeswaran0059@gmail.com>
2026-04-02 15:00:31 +0530

    Initial commit for devops project

---
+++ src/main/java/com/devops/HelloServlet.java
@@ -0,0 +1,20 @@
+package com.devops;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws IOException {
+
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+
+        out.println("<h1>Hello DevOps 🚀</h1>");
+        out.println("<h2>CI/CD Pipeline Working</h2>");
+    }
+}
