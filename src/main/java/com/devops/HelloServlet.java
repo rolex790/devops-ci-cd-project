954d5aff724b3d6f4afb0138dad7d160185f1984
rolex790 <yokeswaran0059@gmail.com>
2026-04-09 18:29:29 +0530

    Update HelloServlet.java

--- src/main/java/com/devops/HelloServlet.java
+++ src/main/java/com/devops/HelloServlet.java
@@ -14,7 +14,7 @@ public class HelloServlet extends HttpServlet {
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
 
-        out.println("<h1>Hello DevOps 🚀</h1>");
+        out.println("<h1>Hello DevOps I am in🚀</h1>");
         out.println("<h2>CI/CD Pipeline Working</h2>");
     }
 }
